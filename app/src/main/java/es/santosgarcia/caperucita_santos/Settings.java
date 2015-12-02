23471537b8fb58d78d57aff85491c9ce5ddbe0b4
Miguel Angel Nuñez Aparicio <miguel.angel.nao@gmail.com>
2015-12-02 18:04:04 +0100

    Todo unido y funcionando

---
+++ app/src/main/java/es/santosgarcia/caperucita_santos/Settings.java
@@ -0,0 +1,25 @@
+package es.santosgarcia.caperucita_santos;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+/**
+ ToggleButton toggle = (ToggleButton) findViewById(R.id.toggleVolume);
+ toggle.OnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+ public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+ if (isChecked) {
+ // The toggle is enabled
+ } else {
+ // The toggle is disabled
+ }
+ }
+ }
+ */
+}
